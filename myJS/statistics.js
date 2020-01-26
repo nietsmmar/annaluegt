95c26c3355079ee4ec5e206a9877489048fe151b
nietsmmar <teabag.pof@gmail.com>
2020-01-26 23:45:20 +0100

    fixed duplicate loadstatistics method. Showing statistics in red which haven't been updated today

--- myJS/statistics.js
+++ myJS/statistics.js
@@ -2,46 +2,52 @@
 var dataPoints = new Object();
 var options =  new Object();
 
-$(function() {
-    function loadStatistics() {
-        $( '#statisticsBox' ).empty();
+function loadStatistics() {
+    $( '#statisticsBox' ).empty();
 
-        attributes.forEach(function(attrId) {
-            dataPoints[attrId] = [];
-            options[attrId] =  {
-            	animationEnabled: true,
-            	theme: "light2",
-            	axisX: {
-            		valueFormatString: "DD MMM YYYY",
-            	},
-            	axisY: {
-            		includeZero: false
-            	},
-            	data: [{
-            		type: "spline",
-            		yValueFormatString: "#,###.##",
-            		dataPoints: dataPoints[attrId]
-            	}]
-            };
-            $.post( "loadStatistics.php", { attrId: attrId })
-            .done(function( data ) {
-                console.log(data);
-                data = $.parseJSON(data);
+    attributes.forEach(function(attrId) {
+        dataPoints[attrId] = [];
+        options[attrId] =  {
+        	animationEnabled: true,
+        	theme: "light2",
+        	axisX: {
+        		valueFormatString: "DD MMM YYYY",
+        	},
+        	axisY: {
+        		includeZero: false
+        	},
+        	data: [{
+        		type: "spline",
+        		yValueFormatString: "#,###.##",
+        		dataPoints: dataPoints[attrId]
+        	}]
+        };
+        $.post( "loadStatistics.php", { attrId: attrId })
+        .done(function( data ) {
+            console.log(data);
+            data = $.parseJSON(data);
 
-                $( '#statisticsBox' ).append("<div id=\"statisticsBox-" + attrId + "\" class=\"col- col-md-auto myhoverable mx-4 my-3 attributeCard\">\
-                <div class=\"boxTitle\">" + attributeNames[attrId] + "</div>"
-                + "<div id=\"statisticsChart-" + attrId + "\" style=\"height: 370px; width: 100%;\"></div>"
-                + "</div>");
+            $( '#statisticsBox' ).append("<div id=\"statisticsBox-" + attrId + "\" class=\"col- col-md-auto myhoverable mx-4 my-3 attributeCard\">\
+            <div class=\"boxTitle\">" + attributeNames[attrId] + "</div>"
+            + "<div id=\"statisticsChart-" + attrId + "\" style=\"height: 370px; width: 100%;\"></div>"
+            + "</div>");
 
-                $.each( data, function( key, value ) {
-                    dataPoints[attrId].push({
-            			x: new Date(value.day),
-            			y: value.value
-            		});
-                });
-
-                $("#statisticsChart-" + attrId).CanvasJSChart(options[attrId]);
+            var loggedToday = false;
+            $.each( data, function( key, value ) {
+                dataPoints[attrId].push({
+        			x: new Date(value.day),
+        			y: value.value
+        		});
+                if (new Date(value.day).setHours(0,0,0,0) == new Date().setHours(0,0,0,0)) {
+                    loggedToday = true;
+                }
             });
+
+            if (!loggedToday) {
+                $( "#statisticsBox-" + attrId ).css("border","solid 1px red");
+            }
+
+            $("#statisticsChart-" + attrId).CanvasJSChart(options[attrId]);
         });
-    }
-});
+    });
+}
